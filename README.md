9ca0a53ce052a6e6e39b18c7fb6e77d3114cd9a9
CarstenFrommhold <carsten.frommhold@datadrivers.de>
2022-12-11 13:00:38 +0100

    day11 - pt2

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Advent of code 2022
 
 ...
+
+## Notes 
+
+
+| Day | Note                                                                    |
+|-----|-------------------------------------------------------------------------|
+| 11  | Kicker idea: least common divisor to avoid exponential computation time |
+| ... | ...                                                                     |
--- solutions/day11.py
+++ solutions/day11.py
@@ -2,9 +2,11 @@ import utils
 from dataclasses import dataclass
 import math
 from typing import Callable
+import time
 
 data = utils.day_puzzle_to_list("11")
 chunks = [data[x:x+7] for x in range(0, len(data), 7)]
+LOG = False
 
 
 @dataclass
@@ -31,8 +33,6 @@ def throw_to(worry_level: int, divisible_by: int, monkey_to_true: int, monkey_to
     else:
         return monkey_to_false
 
-# ['Monkey 0:', '  Starting items: 54, 53', '  Operation: new = old * 3', '  Test: divisible by 2', '    If true: throw to monkey 2', '    If false: throw to monkey 6', '']
-
 
 for monkey_no, chunk in enumerate(chunks):
     for no, row in enumerate(chunk):
@@ -52,25 +52,46 @@ for monkey_no, chunk in enumerate(chunks):
             test_false = eval(row.replace("    If false: throw to monkey ", ""))
     monkeys[monkey_no] = Monkey(items, operation, test_div, test_true, test_false)
 
-print(monkeys)
 
-for round in range(1, 21):
-    for monkey_no, monkey in monkeys.items():
-        while monkey.items != list():  # don't iterate over mutable when you change it.
-            item = monkey.items.pop(0)  # take first item
-            worry_level = item.worry_level
-            worry_level = monkey.operation(worry_level)
-            # Monkey gets bored
-            worry_level = math.floor(worry_level/3)
-            # Throw to another monkey
-            throw_to_monkey = throw_to(worry_level, monkey.test_div, monkey.test_true, monkey.test_false)
-            monkeys[throw_to_monkey].items.append(Item(worry_level))
-            monkey.inspect_items_counter += 1
+divs = [monkey.test_div for monkey in monkeys.values()]
+least_common_divisor = divs[0]
+for n in range(1, len(divs)):
+    least_common_divisor = least_common_divisor * divs[n]
+
+start = time.time()
 
 
-counters = []
-for monkey in monkeys.values():
-    counters.append(monkey.inspect_items_counter)
-counters.sort()
-pt1 = counters[-1] * counters[-2]
-print(pt1)
+def solve(pt: int) -> int:
+    n_rounds = {1: 20, 2: 10000}.get(pt)
+    for round_ in range(1, (n_rounds + 1)):
+
+        if LOG:
+            print("Round", round_)
+            print(f"Time elapsed: {round(time.time() - start), 2} seconds.")
+
+        for monkey_no, monkey in monkeys.items():
+            while monkey.items != list():  # don't iterate over mutable if changing its length or order!
+                # monkey take first item
+                item = monkey.items.pop(0)
+                worry_level = item.worry_level % least_common_divisor
+                worry_level = monkey.operation(worry_level)
+                # monkey gets bored
+                if pt == 1:
+                    worry_level = math.floor(worry_level/3)
+                # monkey throws to another monkey
+                throw_to_monkey = throw_to(worry_level, monkey.test_div, monkey.test_true, monkey.test_false)
+                monkeys[throw_to_monkey].items.append(Item(worry_level))
+                monkey.inspect_items_counter += 1
+
+    counters = []
+    for monkey in monkeys.values():
+        counters.append(monkey.inspect_items_counter)
+    counters.sort()
+    return counters[-1] * counters[-2]
+
+
+pt1 = solve(1)
+pt2 = solve(2)
+
+print("Part 1:", pt1)
+print("Part 2:", pt2)
