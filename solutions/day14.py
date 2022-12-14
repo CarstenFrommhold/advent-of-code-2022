af1d8c91fab2298ed445c91632c1e1341da38ec4
CarstenFrommhold <carsten.frommhold@datadrivers.de>
2022-12-14 14:28:20 +0100

    day 14 refactoring pt 1

--- solutions/day14.py
+++ solutions/day14.py
@@ -5,27 +5,27 @@ from typing import Tuple, List
 data = utils.day_puzzle_to_list("14")
 
 
+@ dataclass
 class Cord:
     x: int
     y: int
-    val: str
 
 
-def pixels_between(start: Tuple[int, int], end: Tuple[int, int]) -> List[Tuple[int, int]]:
-    if start[0] != end[0] and start[1] != end[1]:
+def cords_between(start: Cord, end: Cord) -> List[Cord]:
+    if start.x != end.x and start.y != end.y:
         raise Exception
-    elif start[0] == end[0]:
-        if end[1] >= start[1]:
-            r = range(start[1], end[1] + 1)
+    elif start.x == end.x:
+        if end.y >= start.y:
+            r = range(start.y, end.y + 1)
         else:
-            r = range(end[1], start[1] + 1)
-        return [(start[0], i) for i in r]
-    elif start[1] == end[1]:
-        if end[0] >= start[0]:
-            r = range(start[0], end[0] + 1)
+            r = range(end.y, start.y + 1)
+        return [Cord(start.x, i) for i in r]
+    elif start.y == end.y:
+        if end.x >= start.x:
+            r = range(start.x, end.x + 1)
         else:
-            r = range(end[0], start[0] + 1)
-        return [(i, start[1]) for i in r]
+            r = range(end.x, start.x + 1)
+        return [Cord(i, start.y) for i in r]
 
 
 min_x = 500
@@ -41,7 +41,7 @@ for p in data:
             min_x = min(x, min_x)
             max_x = max(x, max_x)
             max_y = max(y, max_y)
-            cords_filled.append((x, y))
+            cords_filled.append(Cord(x, y))
         else:
             x_prev: int = x
             y_prev: int = y
@@ -50,30 +50,22 @@ for p in data:
             min_x = min(x, min_x)
             max_x = max(x, max_x)
             max_y = max(y, max_y)
-            for between in pixels_between(start=(x_prev, y_prev), end=(x, y)):
+            for between in cords_between(start=Cord(x_prev, y_prev), end=Cord(x, y)):
                 cords_filled.append(between)
 
-print(min_x)
-print(max_x)
-print(max_y)
+cords_filled = [Cord(c.x - min_x + 1, c.y) for c in cords_filled]
 print(cords_filled)
 
-
-cords_filled = [(c[0] - min_x + 1, c[1]) for c in cords_filled]
-print(cords_filled)
-
-# 500, 0 wird zu (500 + 503 - 494)
+# Build Grid
 len_y = max_y
-len_x = max([c[0] for c in cords_filled])
-print(len_x)
-
+len_x = max([c.x for c in cords_filled])
 grid = [["L"] + ["." for _ in range(len_x)] for _ in range(len_y + 1)]
 grid = [l + ["R"] for l in grid]
 grid[0] = ["O" for _ in range(len_x + 2)]
 grid.append(["U" for _ in range(len_x + 2)])
 
 for c in cords_filled:
-    grid[c[1]][c[0]] = "#"
+    grid[c.y][c.x] = "#"
 
 for row in grid:
     print(row)
@@ -81,24 +73,19 @@ for row in grid:
 print(len(grid))
 print(len(grid[1]))
 
-#500, 0 = 7, 0
-#500, 1 = 7, 1
+
+def get_status(candidate: Cord, grid: List[Cord]) -> str:
+    return grid[candidate.y][candidate.x]
 
 
-def get_status(candidate, grid) -> str:
-    return grid[candidate[1]][candidate[0]]
-
-
-def set_snowflake(candidate: Tuple[int, int], grid) -> List[List[str]]:
+def set_snowflake(candidate: Cord, grid: List[Cord]) -> List[Cord]:
     """ snow flake comes from above @ ()"""
-    # if candidate == (500, 1) and get_status(candidate) == "o":
-    #     raise Exception("full")
     if get_status(candidate, grid) in (["L, R, U"]):
-        raise Exception("vorbei")
+        raise Exception("No place for any flakes anymore.")
 
-    under = (candidate[0], candidate[1] + 1)
-    left_under = (candidate[0] - 1, candidate[1] + 1)
-    right_under = (candidate[0] + 1, candidate[1] + 1)
+    under = Cord(candidate.x, candidate.y + 1)
+    left_under = Cord(candidate.x - 1, candidate.y + 1)
+    right_under = Cord(candidate.x + 1, candidate.y + 1)
 
     if get_status(under, grid) not in (["#", "o"]):
         return set_snowflake(under, grid)
@@ -107,11 +94,11 @@ def set_snowflake(candidate: Tuple[int, int], grid) -> List[List[str]]:
     elif get_status(right_under, grid) not in (["#", "o"]):
         return set_snowflake(right_under, grid)
     else:
-        grid[candidate[1]][candidate[0]] = "o"
+        grid[candidate.y][candidate.x] = "o"
         return grid
 
-print(500 - min_x + 1)
-start_ = (500 - min_x + 1, 1)
+
+start_ = Cord(500 - min_x + 1, 1)
 counter = 0
 go_on = True
 while go_on:
@@ -125,4 +112,6 @@ while go_on:
         print(counter)
         go_on = False
 
+pt1 = counter
+print("Part 1:", pt1)
 
